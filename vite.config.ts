330c5c3482a27bb08c663219e07b91d788e3f9f2
200anxy <aadarshnair.p@gmail.com>
2026-03-18 18:35:08 +1100

    chore: Update Vite base path and favicon link to use relative paths.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/prism/',
+  base: './',
   plugins: [
     VitePWA({
       registerType: 'prompt', // We want to control the update prompt
